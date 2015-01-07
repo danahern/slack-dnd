529fdca7d4f7cfeae9157f5718a92832af84cee8
Tyler Bindon (martica) <martica@martica.org>
2015-01-06 22:55:40 -0700

    Add underscore.js and mathjs

--- roll.js
+++ roll.js
@@ -1,27 +1,16 @@
-// Pretty basic stuff. Should be part of Array.
-Array.prototype.sum = function () {
-    return this.reduce(function(a,r) {return a+=r}, 0);
-}
-
-// javascript arrays are sparse when using the constructor Array(size)
-function denseArray(size) {
-    return Array.apply(null, Array(size));
-}
-
-function sign(x) {
-    return (x>0) - (x<0);
-}
+var _ = require('underscore');
+var math = require('mathjs');
 
 // return a random integer
 //   if max = 0 return 0
 //   if max > 0 pick from range 1..max
 //   if max < 0 pick from range max..-1
 function rollDie(max){
-    return sign(max) * (Math.floor( Math.random() * Math.abs(max) ) + 1);
+    return math.sign(max) * _.random(1, math.abs(max));
 }
 
 function rollDice(count, max) {
-    return denseArray(count).map(function(_,_) {return rollDie(max)});
+    return _.map( _.range(count), function(_) {return rollDie(max)});
 }
 
 function applyModifier(total, modifier) {
@@ -47,12 +36,14 @@ function roll( command ) {
         diceSpecs = re.exec("1d20");
     }
 
-    var numDice = parseInt(diceSpecs[1]) || 1; // default to 1 for the /roll d20 case
-    var diceType = parseInt(diceSpecs[2]);
-    var modifier = parseInt(diceSpecs[3]);
+    diceSpecs = _.map(diceSpecs, function(x) {return parseInt(x)});
+
+    var numDice = diceSpecs[1] || 1; // default to 1 for the /roll d20 case
+    var diceType = diceSpecs[2];
+    var modifier = diceSpecs[3];
 
     var results = rollDice(numDice, diceType);
-    var total = results.sum();
+    var total = math.sum(results);
 
     return ' rolled ' + command + ': ' + total + applyModifier(total, modifier) + detailedRolls(results);
 }
